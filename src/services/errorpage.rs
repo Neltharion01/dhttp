6ab304787a7b174376978face42a3d4355646129
Neltharion01 <251054566+Neltharion01@users.noreply.github.com>
2026-01-05 05:25:01 +0300

    Fixed default error page CSS

--- src/services/errorpage.rs
+++ src/services/errorpage.rs
@@ -3,7 +3,7 @@ use crate::reqres::{res, HttpRequest, HttpResponse, StatusCode};
 
 fn error_page(code: u16, code_desc: &str, desc: &str, name: &str) -> String {
 format!(r#"<!doctype html>
-<html><title>{code} {code_desc}</title><meta name="viewport" content="width=device-width"><style>*{{font-family:sans-serif;color:#0e1219;background:#f9f9f9}}@media(prefers-color-scheme:dark){{*{{color:#47d8bb;background:#0e1219}}}}h1{{margin:0;}}div{{position:fixed;top:50%;left:50%;transform:translate(-50%,-50%);padding:8px;border:4px solid;border-color:#0e1219}}@media(prefers-color-scheme:dark){{div{{border-color:#47d8bb}}}}</style><div>
+<html><title>{code} {code_desc}</title><meta name="viewport" content="width=device-width"><style>*{{font-family:sans-serif;color:#0e1219;border-color:#0e1219;background:#f9f9f9}}@media(prefers-color-scheme:dark){{*{{color:#47d8bb;border-color:#47d8bb;background:#0e1219}}}}h1{{margin:0;}}div{{position:fixed;top:50%;left:50%;transform:translate(-50%,-50%);padding:8px;border:4px solid}}</style><div>
 
 <h1>    {code} {code_desc}    </h1>
         {desc}
